cfa93bc5074face32322ae7aafa898f2474ad886
Denise Pendleton <dpendl1@hotmail.com>
2018-05-21 15:46:44 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,10 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    if @author.valid?
-      @author = Author.create!(author_params)
 
+      @author = Author.create!(author_params)
+if @author.valid?
+  @author.save
       redirect_to author_path(@author)
   else
     render :new
